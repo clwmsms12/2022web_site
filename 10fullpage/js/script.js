ff0c3d3ea3069eeea308ee467b5a86606e27e5f5
clwmsms12 <clwmsms12@naver.com>
2022-09-27 18:18:28 +0900

    full27

--- 10fullpage/js/script.js
+++ 10fullpage/js/script.js
@@ -1,1 +1,10 @@
 $("#fullpage").fullpage();
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
